dca9174417c535c3d93c10bdb5b8b54e6786a61a
Petr Saparov <saparov.petr@yandex.ru>
2023-12-11 11:51:02 +0300

    11-12-23

---
+++ previousScript.js
@@ -0,0 +1,71 @@
+const container = document.querySelector('.container');
+const songsContainer = container.querySelector('.songs-container');
+const addButton = container.querySelector('.input__btn_action_add');
+const resetButton = container.querySelector('.input__btn_action_reset');
+const noSongsElement = container.querySelector('.no-songs');
+
+// Эта функция:
+// выключает у кнопки атрибут disabled (делает ее желтой и кликабльной),
+// убирает у кнопки класс .input__btn_disabled,
+// скрывает надпись "нет песен" добавлением класса .no-songs_hidden
+function renderHasSongs() {
+  resetButton.removeAttribute('disabled');
+  resetButton.classList.remove('input__btn_disabled');
+  noSongsElement.classList.add('no-songs_hidden');
+}
+
+// Эта функция  ->  :
+// включает у кнопки атрибут disabled (делает ее желтой и кликабльной)
+// добавляет у кнопке класс .input__btn_disabled
+// показывает надпись "нет песен", удаляя класс .no-songs_hidden
+function renderNoSongs() {
+  resetButton.setAttribute('disabled', true);
+  resetButton.classList.add('input__btn_disabled');
+  noSongsElement.classList.remove('no-songs_hidden');
+}
+
+// Эта функция -> добавляет запись при ее вызове:
+// записывает содержимое тега темплейт в переменную,
+// создает элемент плейлиста и клонирует его (?) 
+// наполняет элемент плейлиста содержимым, введенным в .song__artist
+// наполняет элемент плейлиста содержимым, введенным в .song__title
+// запускает при клике функцию, подставляющую и убирающиую класс .song__like_active в дополнение к классу .song__like
+// отображает сформированный элемент плейлиста последним в блоке с классом .songs-container
+function addSong(artistValue, titleValue) {
+  const songTemplate = document.querySelector('#song-template').content;
+  const songElement = songTemplate.querySelector('.song').cloneNode(true);
+  songElement.querySelector('.song__artist').textContent = artistValue;
+  songElement.querySelector('.song__title').textContent = titleValue;
+  songElement.querySelector('.song__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('song__like_active'); 
+  });
+  songsContainer.append(songElement);
+}
+
+// Слушатель запускает по клику кнопки "Добавить" функцию, которая:
+// записывает в переменную поле ввода .input__text_type_artist
+// записывает в переменную поле ввода .input__text_type_title
+// запускает функцию addSong, передав ей введенное пользователем содержимое параметров value
+// запускает функцию renderHasSongs
+// очищает параметр value у поля ввода .input__text_type_artist
+// очищает параметр value у поля ввода .input__text_type_title
+addButton.addEventListener('click', function () {
+  const artist = document.querySelector('.input__text_type_artist');
+  const title = document.querySelector('.input__text_type_title');
+  addSong(artist.value, title.value); 
+  renderHasSongs();
+  artist.value = '';
+  title.value = '';
+});
+
+// Слушатель запускает по клику кнопки "Очистить" функцию, которая:
+// выбирает все элементы с классом .song
+// запускает цикл удаления элемента, который заканчивается последним элементом 
+// запускает функцию renderNoSongs
+resetButton.addEventListener('click', function () {
+  const songs = document.querySelectorAll('.song')
+  for (let i = 0; i < songs.length; i++) {
+    songs[i].remove();
+  }
+  renderNoSongs();
+});
--- script.js
+++ script.js
@@ -1,96 +1,52 @@
-const container = document.querySelector('.container');
+const songsContainer = document.querySelector('.songs-container');
+const addButton = document.querySelector('.input__btn_action_add');
+const form = document.forms.add; // добавлена форма по ее атрибуту name="add"
+const artist = form.elements.artist; // добавлен элемент формы по его имени
+const title = form.elements.title; // добавлен элемент формы по его имени
 
-const songsContainer = container.querySelector('.songs-container');
-
-const addButton = container.querySelector('.input__btn_action_add');
-
-const resetButton = container.querySelector('.input__btn_action_reset');
-
-const noSongsElement = container.querySelector('.no-songs');
-
-// Эта функция:
-// выключает у кнопки атрибут disabled (делает ее желтой и кликабльной),
-// убирает у кнопки класс .input__btn_disabled,
-// скрывает надпись "нет песен" добавлением класса .no-songs_hidden
-function renderHasSongs() {
-  
-  resetButton.removeAttribute('disabled');
- 
-  resetButton.classList.remove('input__btn_disabled');
- 
-  noSongsElement.classList.add('no-songs_hidden');
-}
-
-
-// Эта функция  ->  :
-// включает у кнопки атрибут disabled (делает ее желтой и кликабльной)
-// добавляет у кнопке класс .input__btn_disabled
-// показывает надпись "нет песен", удаляя класс .no-songs_hidden
-function renderNoSongs() {
-  
-  resetButton.setAttribute('disabled', true);
-  
-  resetButton.classList.add('input__btn_disabled');
-  
-  noSongsElement.classList.remove('no-songs_hidden');
-}
-
-// Эта функция -> добавляет запись при ее вызове:
-// записывает содержимое тега темплейт в переменную,
-// создает элемент плейлиста и клонирует его (?) 
-// наполняет элемент плейлиста содержимым, введенным в .song__artist
-// наполняет элемент плейлиста содержимым, введенным в .song__title
-// запускает при клике функцию, подставляющую и убирающиую класс .song__like_active в дополнение к классу .song__like
-// отображает сформированный элемент плейлиста последним в блоке с классом .songs-container
+// функция , добавляющая песню
 function addSong(artistValue, titleValue) {
   const songTemplate = document.querySelector('#song-template').content;
-  
-  const songElement = songTemplate.querySelector('.song').cloneNode(true);
-  
+  const songElement = songTemplate.cloneNode(true);
   songElement.querySelector('.song__artist').textContent = artistValue;
- 
   songElement.querySelector('.song__title').textContent = titleValue;
-  
-  songElement.querySelector('.song__like').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('song__like_active'); 
-  });
-  
   songsContainer.append(songElement);
 }
 
-// Слушатель запускает по клику кнопки "Добавить" функцию, которая:
-// записывает в переменную поле ввода .input__text_type_artist
-// записывает в переменную поле ввода .input__text_type_title
-// запускает функцию addSong, передав ей введенное пользователем содержимое параметров value
-// запускает функцию renderHasSongs
-// очищает параметр value у поля ввода .input__text_type_artist
-// очищает параметр value у поля ввода .input__text_type_title
-addButton.addEventListener('click', function () {
-  
-  const artist = document.querySelector('.input__text_type_artist');
-  
-  const title = document.querySelector('.input__text_type_title');
-  
+// функция, отключающая за активацию стилизацию кнопки Добавить
+function setSubmitButtonState(isFormValid) {
+  if (isFormValid) {
+    addButton.removeAttribute('disabled');
+    addButton.classList.remove('input__btn_disabled');
+  } else {
+    addButton.setAttribute('disabled', true);
+    addButton.classList.add('input__btn_disabled');
+  }
+}
+
+// слушатель отправки формы, который:
+// - сбрасывает поведение браузера по умолчанию,
+// - добавляет песню, запуская функцию addSong,
+// - очищает форму,
+// - поерестилизовывает кнопку отправки после события
+form.addEventListener('submit', function (evt) {
+  evt.preventDefault();
   addSong(artist.value, title.value);
- 
-  renderHasSongs();
-  
-  artist.value = '';
-  
-  title.value = '';
+	form.reset();
+  setSubmitButtonState(false);
 });
 
-// Слушатель запускает по клику кнопки "Очистить" функцию, которая:
-// выбирает все элементы с классом .song
-// запускает цикл удаления элемента, который заканчивается последним элементом 
-// запускает функцию renderNoSongs
-resetButton.addEventListener('click', function () {
- 
-  const songs = document.querySelectorAll('.song')
-  
-  for (let i = 0; i < songs.length; i++) {
-    songs[i].remove();
+// слушатель поля ввода, который:
+// - прверяет наличие  в ней символов,
+// - при заполнении, делает кнопку Добавить активной
+form.addEventListener('input', function (evt) {
+  const isValid = artist.value.length > 0 && title.value.length > 0;
+  setSubmitButtonState(isValid);
+});
+
+// слушатель, делегированный родителю, переключающий лайк
+songsContainer.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains('song__like')) {
+    evt.target.classList.toggle('song__like_active');
   }
-  
-  renderNoSongs();
 });
